017c0469f6107f2ae5ecfbb04c382228b864d25e
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-09-05 22:33:39 +0200

    WIP

--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,22 @@ source_parsers = {
 
 source_suffix = ['.rst', '.md']
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 
 html_theme_options = {
     'collapse_navigation': False,
     'display_version': False,
     'navigation_depth': 3,
 }
+
+
+master_doc = 'index'
+project = u'Scalameta'
+copyright = u'2014-2017, EPFL'
+author = u'Eugene Burmako, Olafur Pall Geirsson'
+
+html_logo = '_static/logo.png'
+latex_documents = [
+    (master_doc, 'Scalameta.tex', u'Scalameta Documentation',
+     u'Eugene Burmako, Olafur Pall Geirsson', 'manual'),
+]
